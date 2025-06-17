55e35fe6c08278081cca7e19bd1b265fc42e50da
Elliot Carr <Elliot.Carr@navtor.com>
2025-06-17 15:03:41 +0100

    refactor auth store

--- src/app/store/auth/auth.store.ts
+++ src/app/store/auth/auth.store.ts
@@ -1,53 +1,58 @@
-import {patchState, signalStore, withComputed, withMethods, withState} from '@ngrx/signals'
+import {patchState, signalStore, withMethods, withProps, withState} from '@ngrx/signals'
 import { initialAuthSlice } from './auth.slice'
 import { inject } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
-import { catchError, EMPTY, finalize, tap } from 'rxjs';
+import { catchError, EMPTY, switchMap, tap } from 'rxjs';
 import { Router } from '@angular/router';
 import { User } from '../../models/User';
 import { UserStore } from '../user/user.store';
 import { WalkStore } from '../walk/walks.store';
+import { rxMethod } from '@ngrx/signals/rxjs-interop';
+import { setError, setLoading } from './auth.updaters';
+import { LoginRequest } from '../../models/LoginReq';
 
 export const AuthStore = signalStore(
     {providedIn:'root'},
     withState(initialAuthSlice),
 
-    withComputed((store)=>{
-        return{
+    withProps(_=>({
+        _walkStore : inject(WalkStore),
+        _userStore : inject(UserStore),
+        _authService: inject(AuthService),
+        _router: inject(Router)
+    })),
 
-        }
-    }),
-
-    withMethods((store, authService = inject(AuthService), router = inject(Router),) => {
-        const userStore = inject(UserStore)
-        const walkStore = inject(WalkStore)
-        const loginRequest = (username: string, password:string) => {
-            patchState(store, ()=> ({loading: true}));
-
-            if(!username || !password){
-                patchState(store, ()=> ({error: 'Username and Password are required'}));
-                return;
-            } 
-
-            authService.login(username, password).pipe(
-                tap((user:User)=>{
-                    userStore.setUser(user)
-                    walkStore.loadWalks()
-                    router.navigate(['home'])
-                }),
-                finalize(()=>{
-                    patchState(store, ()=> ({loading: false}))
+    withMethods((store) => {
+        const loginRequest = rxMethod<LoginRequest>(req =>{
+            const output = req.pipe(
+                tap(()=> patchState(store, setLoading(true))),
+                switchMap((req) => store._authService.login(req.username, req.password)),
+                tap((user:User)=> {
+                    
+                    store._userStore.setUser(user)
+                    store._walkStore.loadWalks()
+                    store._router.navigate(['home'])
+                    patchState(store, setLoading(false))
                 }),
                 catchError(err=>{
                     const errorMsg = err.error?.error;
-                    patchState(store, ()=>({error: errorMsg}))
+                    patchState(store, setError(errorMsg))
                     return EMPTY;
                 })
-            ).subscribe()
+            )
+            return output
+        })
+
+        const performLogin = (request:LoginRequest) => {
+            if(!request.username || !request.password){
+                patchState(store, setError('Username and Password are required'));
+                return;
+            }
+            loginRequest(request)
         }
 
         return{
-            loginRequest
+            performLogin
         }
     })
 )
---
+++ src/app/store/auth/auth.updaters.ts
@@ -0,0 +1,10 @@
+import { PartialStateUpdater } from "@ngrx/signals";
+import { AuthSlice } from "./auth.slice";
+
+export function setLoading(loading: boolean): PartialStateUpdater<AuthSlice>{
+    return _ => ({loading})
+}
+
+export function setError(error:string): PartialStateUpdater<AuthSlice>{
+    return _ => ({error})
+}
