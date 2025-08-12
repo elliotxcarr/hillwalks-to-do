cfe72224cd9b3716c4c5226fa00deea14872b132
Elliot Carr <Elliot.Carr@navtor.com>
2025-08-12 11:35:42 +0100

    login refactor

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,14 +1,14 @@
-import { Component, computed, inject, Signal} from '@angular/core';
+import { Component, inject} from '@angular/core';
 import '@angular/forms'
 import { FormsModule } from '@angular/forms';
-import { NgClass, NgIf } from '@angular/common';
+import { NgClass} from '@angular/common';
 import { AuthStore } from '../store/auth/auth.store';
 import { LoginRequest } from '../models/LoginReq';
 import { Dispatcher } from '@ngrx/signals/events';
 
 @Component({
   selector: 'app-login-page',
-  imports: [ FormsModule, NgIf, NgClass],
+  imports: [ FormsModule, NgClass],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
