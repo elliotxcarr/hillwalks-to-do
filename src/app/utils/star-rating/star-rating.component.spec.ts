7596cdb29e0b65b8e18d42ba6b2f8a56ccd0f02e
Elliot Carr <Elliot.Carr@navtor.com>
2025-08-13 11:57:19 +0100

    unit test fix

--- src/app/utils/star-rating/star-rating.component.spec.ts
+++ src/app/utils/star-rating/star-rating.component.spec.ts
@@ -2,25 +2,10 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { StarRatingComponent } from './star-rating.component';
 import { provideHttpClient } from '@angular/common/http';
-import { Walk } from '../../models/Walk';
 
 describe('StarRatingComponent', () => {
   let component: StarRatingComponent;
   let fixture: ComponentFixture<StarRatingComponent>;
-  let currentWalk = {
-      _id: '1',
-      name: 'Name',
-      location: 'Location',
-      difficulty: 3,
-      rating: 4,
-      startPoint: 'Start Point',
-      hillType: 'Munro',
-      image: 'image Url',
-      duration: '4 Hours',
-      bogFactor: 3,
-      distance: 1,
-      completed: false
-    };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
